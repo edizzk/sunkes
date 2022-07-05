fff7a674af0d335b4d048f4d98c90d2e3451ab7d
edizzk <edizmetekurt@gmail.com>
2022-07-05 16:29:18 +0300

    create basic pages

---
+++ src/pages/CreatePlan.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CreatePlan() {
+  return (
+    <div className='createplan'>
+      <h1>CreatePlan</h1>
+    </div>
+  );
+}
+
+export default CreatePlan;
---
+++ src/pages/ViewPlan.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ViewPlan() {
+  return (
+    <div className='viewplan'>
+      <h1>ViewPlan</h1>
+    </div>
+  );
+}
+
+export default ViewPlan;
