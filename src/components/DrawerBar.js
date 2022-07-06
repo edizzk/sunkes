996aea0eedd103006b198e14744907219b85c5a1
edizzk <edizmetekurt@gmail.com>
2022-07-06 15:14:48 +0300

    add title bar icons

--- src/components/DrawerBar.js
+++ src/components/DrawerBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
+import * as VscIcons from 'react-icons/vsc';
 import { Link } from 'react-router-dom';
 import { SidebarData } from './SidebarData';
 import './DrawerBar.css';
@@ -10,19 +10,33 @@ function DrawerBar() {
   const [sidebar, setSidebar] = useState(false);
 
   const showSidebar = () => setSidebar(!sidebar);
-
+  
   return (
     <>
       <IconContext.Provider value={{ color: '#fff' }}>
         <div className='drawerbar'>
-          <Link to='#' className='menu-bars'>
-            <FaIcons.FaBars onClick={showSidebar} />
-          </Link>
+          <div>
+            <Link to='#' className='menu-bars'>
+              <VscIcons.VscThreeBars onClick={showSidebar} />
+            </Link>
+          </div>
+          <div>
+            <Link to='#' className='minimize-bar'>
+              <VscIcons.VscChromeMinimize onClick={minimizeScreen} />
+            </Link>
+            <Link to='#' className='maximize-bar'>
+              <VscIcons.VscChromeMaximize onClick={maximizeScreen} />
+              {/* VscChromeRestore (EKRAN BÜYÜDÜKTEN SONRA BU İCON A DÖNÜŞECEK--------------------------------------------------------) */}
+            </Link>
+            <Link to='#' className='close-bar'>
+              <VscIcons.VscChromeClose onClick={closeSreen} />
+            </Link>
+          </div>
         </div>
         <nav className={sidebar ? 'drawer-menu active' : 'drawer-menu'}>
           <ul className='drawer-menu-items' onClick={showSidebar}>
             <li className='drawerbar-toggle'>
-              <Link to='#' className='menu-bars'>
+              <Link to='#' className='menu-bars-open'>
                 <AiIcons.AiOutlineClose />
               </Link>
             </li>
